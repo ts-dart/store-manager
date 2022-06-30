d9043c84f6fad909379a835d43b6f17da2daf152
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-06-30 14:50:15 -0300

    testando

---
+++ tests/unit/models/productModels.test.js
@@ -0,0 +1,5 @@
+const { expect } = require('chai');
+
+describe('Testa se pega todos os produtos', () => { });
+describe('Testa se pega um produco com um id correspondente', () => { });
+describe('Testa se e possivel inserit um novo produto', () => { });
