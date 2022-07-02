62c4216bd856ec214014914ce88cf003a8660ad9
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-02 14:09:59 -0300

    ATUALIZANDO

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2/promise');
 
-/* const dbProducts = mysql.createPool({
+const dbProducts = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
-}); */
+});
 
-const dbProducts = mysql.createPool({
+/* const dbProducts = mysql.createPool({
   host: 'localhost',
   user: 'thiago',
   password: 'minhasenhaA1$',
-});
+}); */
 
 module.exports = dbProducts;
